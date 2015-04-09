cb7bef468bd8ed96a3bd7f57cb9d9d0e7b2f9382
Rodrigo <rlopez@rlm.com.mx>
2015-04-09 11:05:15 -0500

    CAmbios icatmor

--- www/js/main.js
+++ www/js/main.js
@@ -93,7 +93,7 @@ function showResultsScreen(){
   // DEcrypt
 
   if(!cancelled){
-     var frase = "X45RTY712";
+     var frase = "X45RTY713";
 
     var cutURL = String(resString.slice(0,resString.indexOf("?")))
 
