113980967679db3b9a2737df9a44a44d01d7d0ef
Rodrigo <rlopez@rlm.com.mx>
2015-05-26 13:24:13 -0500

    cambios cobaez
    cobaez

--- www/js/main.js
+++ www/js/main.js
@@ -93,7 +93,7 @@ function showResultsScreen(){
   // DEcrypt
 
   if(!cancelled){
-     var frase = "X45RTY716";
+     var frase = "X45RTY717";
 
     var cutURL = String(resString.slice(0,resString.indexOf("?")))
 
