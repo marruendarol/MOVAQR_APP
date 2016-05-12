d99a6a94ff4df8d2b5640f67261c8dac67730c2c
Rodrigo <rlopez@rlm.com.mx>
2016-05-12 18:09:28 -0500

    ICATEY

--- www/js/main.js
+++ www/js/main.js
@@ -94,7 +94,7 @@ function showResultsScreen(){
 
   if(!cancelled){
 
-     var frase = "X45RTY711";
+     var frase = "X45RTYA30";
 
 
     var cutURL = String(resString.slice(0,resString.indexOf("?")))
