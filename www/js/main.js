ae186f96e0ef89b96343febea9bd1005cce5f652
Rodrigo <rlopez@rlm.com.mx>
2016-11-05 14:21:49 -0600

    App Michoacan
    Michoacan

--- www/js/main.js
+++ www/js/main.js
@@ -94,7 +94,7 @@ function showResultsScreen(){
 
   if(!cancelled){
 
-     var frase = "X45RTYA32";
+     var frase = "X45RTYA33";
 
 
     var cutURL = String(resString.slice(0,resString.indexOf("?")))
